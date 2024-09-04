01b056372b1efcd31ef70d0d5558a9c2fbae5a22
xiwan <5155280@qq.com>
2024-09-04 09:09:11 +0800

    add config ini

--- Assets/Scripts/Networking/Client/GameLiftClient.cs
+++ Assets/Scripts/Networking/Client/GameLiftClient.cs
@@ -16,9 +16,15 @@ using Amazon.GameLift.Model;
 using Mirror;
 using TMPro;
 using System.Text.RegularExpressions;
+using static UnityEngine.Analytics.IAnalytic;
+using System.IO;
+using UnityEditor.Rendering;
+using UnityEngine.SocialPlatforms;
 
 public class GameLiftClient : MonoBehaviour
 {
+  private Dictionary<string, Dictionary<string, string>> iniData = new Dictionary<string, Dictionary<string, string>>();
+
   static string ipPattern = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
   public TMP_InputField inputAddressField;
   public GameObject loginObject;
@@ -49,17 +55,21 @@ public class GameLiftClient : MonoBehaviour
   void Start()
   {
     Debug.Log("client");
+
+    LoadIniFile("Config.ini");
+    Dictionary<string, string> localConfig = iniData["aws"];
+    bool is_local = bool.Parse(localConfig["local"]);
+    string aws_ak_v = localConfig["aws_ak_v"];
+    string aws_sk_v = localConfig["aws_sk_v"];
+    string aws_gamelift_feet_name = localConfig["aws_gamelift_feet_name"];
+    Debug.Log($"{is_local}, {aws_ak_v}, {aws_sk_v}, {aws_gamelift_feet_name}");
+
+    local = is_local;
+    aws_ak = aws_ak_v;
+    aws_sk = aws_sk_v;
+    fleet_name = aws_gamelift_feet_name;
+
     UIUpdate(true);
-    var config = new AmazonGameLiftConfig();
-    if (local)
-      config.ServiceURL = "http://localhost:7778";
-    else
-      config.RegionEndpoint = RegionEndpoint.USWest2;
-
-    client = new AmazonGameLiftClient(aws_ak, aws_sk, config);
-    playerId = Guid.NewGuid().ToString();
-
-    //Quickplay();
   }
 
   void UIUpdate(bool status)
@@ -74,6 +84,10 @@ public class GameLiftClient : MonoBehaviour
     if (Regex.IsMatch(gameIp, ipPattern))
     {
       Quickplay(gameIp);
+    }
+    else if (gameIp.Equals("localhost") || string.IsNullOrEmpty(gameIp))
+    {
+      Quickplay("127.0.0.1");
     }
     else
     {
@@ -91,6 +105,24 @@ public class GameLiftClient : MonoBehaviour
   {
     var fleets = new List<string>();
     var remote_fleed_id = "";
+    if (gameIp.Equals("127.0.0.1"))
+    {
+      local = true;
+    }
+    else
+    {
+      local = false;
+    }
+
+    var config = new AmazonGameLiftConfig();
+    if (local)
+      config.ServiceURL = "http://localhost:7778";
+    else
+      config.RegionEndpoint = RegionEndpoint.USWest2;
+
+    client = new AmazonGameLiftClient(aws_ak, aws_sk, config);
+    playerId = Guid.NewGuid().ToString();
+
     if (!local)
     {
       fleets = await GetFleets();
@@ -120,16 +152,6 @@ public class GameLiftClient : MonoBehaviour
         networkManager.StartClient();
       }
     }
-    //var sessionId = sessions.FirstOrDefault(s => {
-    //  Debug.Log(gameIp);
-    //  Debug.Log(s.IpAddress);
-    //  return s.Status == GameSessionStatus.ACTIVE && s.IpAddress == gameIp;
-    //}).GameSessionId;
-
-    //currentPlayerSession = await CreatePlayerSessionAsync(sessionId);
-    //Debug.Log($"Successfully connected to session {currentPlayerSession.GameSessionId} at [{currentPlayerSession.DnsName}] {currentPlayerSession.IpAddress}:{currentPlayerSession.Port}");
-    //networkManager.networkAddress = currentPlayerSession.IpAddress;
-    //networkManager.StartClient();
   }
 
   async Task<List<string>> GetFleets(CancellationToken token = default)
@@ -151,6 +173,53 @@ public class GameLiftClient : MonoBehaviour
   {
     var response = await client.CreatePlayerSessionAsync(gameSessionId, playerId, token);
     return response.PlayerSession;
+  }
+
+  void LoadIniFile(string filename)
+  {
+#if UNITY_EDITOR
+    string configFilePath = Path.Combine(Application.streamingAssetsPath, filename);
+#else
+    string configFilePath = Path.Combine ("./", filename);
+#endif
+    Debug.Log(configFilePath);
+
+    iniData.Clear();
+
+    if (File.Exists(configFilePath))
+    {
+      string[] lines = File.ReadAllLines(configFilePath);
+
+      string currentSection = null;
+      Dictionary<string, string> currentSectionData = null;
+
+      foreach (string line in lines)
+      {
+        string trimmedLine = line.Trim();
+
+        if (trimmedLine.StartsWith("[") && trimmedLine.EndsWith("]"))
+        {
+          currentSection = trimmedLine.Substring(1, trimmedLine.Length - 2);
+          currentSectionData = new Dictionary<string, string>();
+          iniData[currentSection] = currentSectionData;
+        }
+        else if (!string.IsNullOrWhiteSpace(trimmedLine) && currentSectionData != null)
+        {
+          int equalIndex = trimmedLine.IndexOf('=');
+          if (equalIndex >= 0)
+          {
+            string key = trimmedLine.Substring(0, equalIndex).Trim();
+            string value = trimmedLine.Substring(equalIndex + 1).Trim();
+            currentSectionData[key] = value;
+          }
+        }
+      }
+      Debug.Log("配置加载完成" + configFilePath);
+    }
+    else
+    {
+      Debug.LogError("Config file not found: " + configFilePath);
+    }
   }
 
 }
